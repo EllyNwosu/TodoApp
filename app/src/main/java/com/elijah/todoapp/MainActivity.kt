3d2c33e1a80b65d8dfbff94de78703ba07b9800f
ellynwosu <elijahevergreen@gmail.com>
2021-06-22 17:27:05 +0100

    intent use

--- app/src/main/java/com/elijah/todoapp/MainActivity.kt
+++ app/src/main/java/com/elijah/todoapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.elijah.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+
+val validUsername = "admin"
+val validPassword = "admin"
+
+private lateinit var usernameInput: EditText
+private lateinit var passwordInput: EditText
+private lateinit var submitButton: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.userNameInput)
+        passwordInput = findViewById(R.id. passwordInput)
+        submitButton = findViewById(R.id.submitButtonInput)
+
+        submitButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Login(username, password)
+        }
+    }
+
+    private fun Login(username: String, password: String) {
+        if (isValidCredentials(username, password)){
+            val intent: Intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("name", username)
+            startActivity(intent)
+        }else{
+            Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun isValidCredentials(username: String, password: String): Boolean {
+         if(username == validUsername && password == validPassword)  return true
+        return false
+
     }
 }
